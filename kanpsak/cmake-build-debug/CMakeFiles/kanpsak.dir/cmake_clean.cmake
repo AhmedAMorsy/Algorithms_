8a46b428d2ecf57c9546760b130e873cd887da50
AhmedAMorsy <72940929+AhmedAMorsy@users.noreply.github.com>
2022-12-20 19:13:00 +0200

    Add files via upload

---
+++ kanpsak/cmake-build-debug/CMakeFiles/kanpsak.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kanpsak.dir/main.cpp.obj"
+  "kanpsak.exe"
+  "kanpsak.exe.manifest"
+  "kanpsak.pdb"
+  "libkanpsak.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kanpsak.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
